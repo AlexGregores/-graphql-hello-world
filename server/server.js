60c92df7afc3075580fab64d50b02fcdaddb00b3
AlexGregores <alexandregregores@gmail.com>
2022-07-19 16:22:44 -0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  schema {
+    query: Query
+  }
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await server.listen({ port: 7000 });
+console.log(`Server running at ${url}`);
